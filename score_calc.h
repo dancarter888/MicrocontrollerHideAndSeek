c0768fdca9df65fc5fd63571ad859daeff937895
Daniel Siemers <dps77@uclive.ac.nz>
2019-10-18 01:44:03 +1300

    Working version

---
+++ score_calc.h
@@ -0,0 +1,17 @@
+/**
+@file       score_calc.h
+@authors    Daniel Siemers (45944203) & Ann Ngo (39979230)
+@date       18 October 2019
+@brief      Hide And Seek
+            Score calculation header file.
+**/
+
+#include <stdlib.h>
+
+#ifndef SCORE_CALC_H
+#define SCORE_CALC_H
+
+int check_overlap(int tlx, int tly, int p2tlx, int p2tly);
+
+#endif
+
